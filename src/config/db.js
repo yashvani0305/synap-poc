f8646b077f9fd7d44859753b8193679a99e2e27f
Yash Vani <yash.vani@reynasolutions.com>
2026-04-06 18:31:05 +0530

    logs removed

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,6 @@
 import { configDotenv } from "dotenv";
 import postgres from "postgres";
 configDotenv();
-const connectionString = process.env.DATABASE_URL;
-console.log("Database -------", connectionString);
 import { createClient } from "@supabase/supabase-js";
 
 const SQL = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
